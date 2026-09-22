4904d80e6ecb9249ce797bece8f7d154fe7fe9be
kadoka <tomiya3499@gmail.com>
2026-09-22 10:48:12 +0900

    feat(dsl): export parser and serializer API

--- image_drawer/dsl/__init__.py
+++ image_drawer/dsl/__init__.py
@@ -1,1 +1,17 @@
-"""Workflow DSL parser and serializer package."""
+"""Workflow DSL parser and canonical serializer."""
+
+from image_drawer.dsl.parser import (
+    DslError,
+    DslSyntaxError,
+    DslValidationError,
+    parse_workflow,
+)
+from image_drawer.dsl.serializer import serialize_workflow
+
+__all__ = [
+    "DslError",
+    "DslSyntaxError",
+    "DslValidationError",
+    "parse_workflow",
+    "serialize_workflow",
+]
