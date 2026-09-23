b5483d33586dc95be1cdf46d0687aa941c9aac6c
kadoka <tomiya3499@gmail.com>
2026-09-24 08:14:22 +0900

    feat(search): export parameter search APIs

--- image_drawer/search/__init__.py
+++ image_drawer/search/__init__.py
@@ -12,6 +12,21 @@ from image_drawer.search.models import (
     WorkflowMetrics,
     WorkflowSnapshot,
 )
+from image_drawer.search.parameter_search import (
+    ObjectiveSpec,
+    ParameterDimension,
+    ParameterSearchResult,
+    ParameterSearchRunner,
+    SearchCandidate,
+    SearchSpec,
+    apply_parameter_configuration,
+    export_parameter_search,
+    generate_candidates,
+    generate_configurations,
+    load_parameter_search,
+    reconstruct_best_workflow,
+    score_objective,
+)
 from image_drawer.search.prompt_sets import load_prompt_set
 from image_drawer.search.results import export_experiment, load_experiment
 
@@ -20,11 +35,24 @@ __all__ = [
     "ExperimentRun",
     "ExperimentRunner",
     "ExperimentSpec",
+    "ObjectiveSpec",
+    "ParameterDimension",
+    "ParameterSearchResult",
+    "ParameterSearchRunner",
     "PromptCase",
     "WorkflowMetrics",
+    "SearchCandidate",
+    "SearchSpec",
     "WorkflowSnapshot",
     "aggregate_workflow_metrics",
+    "apply_parameter_configuration",
     "export_experiment",
+    "export_parameter_search",
+    "generate_candidates",
+    "generate_configurations",
     "load_experiment",
+    "load_parameter_search",
     "load_prompt_set",
+    "reconstruct_best_workflow",
+    "score_objective",
 ]
